7d9c598b5ad525486d7970753c952a85200993c3
= <sdvelu18@gmail.com>
2020-10-07 14:41:03 -0400

    Updates

---
+++ precache-manifest.20791ee1ff3d25f9c93db9abd341d0cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84be6b52c5cca8734bda98a968fee2d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9af4522c0635c820359",
+    "url": "/static/css/main.1e74f38c.chunk.css"
+  },
+  {
+    "revision": "6362029f5ee1308db144",
+    "url": "/static/js/2.01394220.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.01394220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9af4522c0635c820359",
+    "url": "/static/js/main.57adcb9a.chunk.js"
+  },
+  {
+    "revision": "83954c0bdb617e4e53bd",
+    "url": "/static/js/runtime-main.36e38421.js"
+  }
+]);
