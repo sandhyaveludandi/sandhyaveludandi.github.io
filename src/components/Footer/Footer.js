e89ed87ba2de71d8590a206e70cb95dcb13957ef
= <sdvelu18@gmail.com>
2020-12-23 22:34:29 -0500

    adjusting footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,8 +11,11 @@ export default function Footer() {
         <div>
             <footer>
                 <h1 class="flex-container">
+                <a href="mailto:sdvelu18@gmail.com">Link text</a>
                     <a href="https://google.com"><FontAwesomeIcon className="icon" icon={faPhoneSquareAlt} /></a>
-                    <a href="https://google.com"><FontAwesomeIcon className="icon" icon={faEnvelope} /></a>
+                    <a href="mailto:sdvelu18@gmail.com?
+                             cc=sv549@scarletmail.rutgers.edu&
+                             subject=Greeting Email"><FontAwesomeIcon className="icon" icon={faEnvelope} /></a>
                     <a href="https://www.linkedin.com/in/sandhyaveludandi/"><FontAwesomeIcon className="icon" icon={faLinkedin} /></a>
                     <a href="https://github.com/sandhyaveludandi"><FontAwesomeIcon className="icon" icon={faGithubSquare} /></a>
                 </h1>
